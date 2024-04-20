90e4e5d5ad67159de704640d5e26df84bc72f68f
Manya <123743851+manya16102003@users.noreply.github.com>
2024-04-20 22:40:49 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here below
-var API_ENDPOINT = "https://9s4i2v769c.execute-api.ap-south-1.amazonaws.com/hehe-api";
+var API_ENDPOINT = "https://pzwl8pjjob.execute-api.us-east-1.amazonaws.com/1st";
 
 // AJAX POST REQUEST
 document.getElementById("saveprofile").onclick = function () {
